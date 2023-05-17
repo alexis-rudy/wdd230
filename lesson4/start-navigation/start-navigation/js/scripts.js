c92878a21cacfc27c482073884f5475cdca8863f
alexis rudy <rud21003@byui.edu>
2023-05-16 21:52:11 -0600

    finished button nav tut

---
+++ lesson4/start-navigation/start-navigation/js/scripts.js
@@ -0,0 +1,8 @@
+const x = document.getElementById("hamburgerBtn");
+x.onclick = toggleMenu();
+
+function toggleMenu(){
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+    
+}
